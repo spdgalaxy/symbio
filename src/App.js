4edd09725ac9a492775dba976b6266bc029a413e
Senthil Kannan <senthil@bluemind.app>
2023-09-08 00:16:41 +0530

    Change3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to SRI Services
+          Welcome to SRI Services Portal
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
       </header>
     </div>
